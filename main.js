e04e08e36cd968be8f5fd57c360291d148dac92a
miloCODE <juamcamirodri@gmail.com>
2024-02-10 02:03:46 -0500

    audio-bar added

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const playButton = document.querySelector(".play i");
   const audio = document.querySelector('audio');
+  const progressBar = document.getElementById('progressBar');
+  const minutosCorridos = document.getElementById('minutosCorridos');
 
   playButton.addEventListener("click", function () {
     const icon = this;
@@ -14,5 +16,27 @@ document.addEventListener("DOMContentLoaded", function () {
       icon.classList.add("fa-circle-play");
       audio.pause();
     }
+  });
+
+  progressBar.addEventListener('input', function() {
+    const percent = this.value;
+    const duration = audio.duration;
+    audio.currentTime = (percent / 100) * duration;
+  });
+
+  audio.addEventListener('timeupdate', function() {
+    const currentTime = audio.currentTime;
+    const duration = audio.duration;
+    const percent = (currentTime / duration) * 100;
+  
+    progressBar.value = percent;
+  
+    // Cambiar el color de fondo de la barra de progreso según el porcentaje completado
+    const gradientColor = `linear-gradient(to right, #1ed760 0%, #1ed760 ${percent}%, #4D4D4D ${percent}%, #4D4D4D 100%)`;
+    progressBar.style.background = gradientColor;
+  
+    const minutes = Math.floor(currentTime / 60);
+    const seconds = Math.floor(currentTime % 60);
+    minutosCorridos.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   });
 });
